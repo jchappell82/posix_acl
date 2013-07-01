c270d7f5b82d918d492a7239d5819f7af286e627
Jon Chappell <jon@jchome.us>
2013-07-01 08:56:14 -0400

    Commentary from the peanut gallery.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,19 @@
-
-
-
-
-
+#
+# Cookbook Name:: posix_acl
+# Attributes:: default
+#
+# Copyright 2013, Jon Chappell.
+#
+# All rights reserved - Do Not Redistribute
+#
 
 # This is the full path to the fstab file you are using.
 default['posix_acl']['fstab_path'] = "/etc/fstab"
 # This is the full path to the backup file you wish to create.
 default['posix_acl']['fstab_backup'] = "/etc/fstab.bak"
 # This is the physical device (or partition label) you wish for the cookbook
-# to enable ACLs on.  e.g. - "/dev/sda1" or "LABEL=server-store"
+# to enable ACLs on.  e.g. - "/dev/sda1" (or just "sda1" is ok)
+# or "LABEL=server-store
 default['posix_acl']['mount_point_device'] = "LABEL=cloudimg-rootfs"
 # This is the actual mount point you are enabling ACLs on.
 default['posix_acl']['mount_point'] = "/"
