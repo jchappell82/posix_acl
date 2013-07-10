ffc6ed5428335d286234049ea085af7324f83708
Jon Chappell <jon@jchome.us>
2013-07-10 14:24:32 -0400

    Version bump, and License change to Apache 2.0.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'posix_acl'
 maintainer       'Jon Chappell'
 maintainer_email 'jon@jchome.us'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures posix_acl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
